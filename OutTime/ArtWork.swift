4c3614d651c26c3c8a115c2b050d44e4fb03bc05
Lazar <lakiando@outlook.com>
2016-11-18 14:12:08 +0100

    dodao sam komentare za sve sto sam koristio i u ArtWork i OutTime!

--- OutTime/ArtWork.swift
+++ OutTime/ArtWork.swift
@@ -9,7 +9,7 @@
 import Foundation
 import MapKit
 
-
+//napravio sam posebnu klasu i pomocu nje cu imati pocetnu tacku sa centrom na osnovu kordinata i tokom klika na nju ispisace se svi detalji te tacke!
 class ArtWork : NSObject, MKAnnotation {
     
     let title : String?
@@ -32,19 +32,23 @@ class ArtWork : NSObject, MKAnnotation {
     }
 }
 
-
+//kreirao sam instancu od te klase i dodelio sam joj sve parametre!
 let artWork = ArtWork(title: "Republic Square", locationName: "Belgrade Center", discipline: "Sculpture", coordinate: CLLocationCoordinate2D(latitude: 44.816088, longitude: 20.459962))
 
+
+
 extension OutTime : MKMapViewDelegate {
-    func mapView(mapVIew: MKMapView!, viewForAnnotation annotation:MKAnnotation) -> MKAnnotationView! {
+    //mapView(_:viewForAnnotation:) poziva svaki annotation koji smo dali mapi da bih dobili prikaz za svaki annotation
+    private func mapView(mapVIew: MKMapView!, viewForAnnotation annotation:MKAnnotation) -> MKAnnotationView! {
         if let annotation = annotation as? ArtWork {
             let indentifier = "pin"
             var view : MKAnnotationView
             if let dequeuedView = mapVIew.dequeueReusableAnnotationView(withIdentifier: indentifier)! as? MKAnnotationView {
-                
+                //mape su tako podesene da one stave znak oznacavanja i ako objekta vise nema. Kod prvo proverava da li je za vecu upotrebu anntotion i onda kreira nov!
                 dequeuedView.annotation = annotation
                 view = dequeuedView
             } else {
+                //ovde koristimo MKAnnotation klasu ako annotation view nije dequeued. Koristi oblacic koji izadje kad korisnik pritisne na pin da ispise naslov
                 view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: indentifier)
                 view.canShowCallout = true
                 view.calloutOffset = CGPoint(x: -5, y: 5)
--- OutTime/OutTime.swift
+++ OutTime/OutTime.swift
@@ -15,12 +15,14 @@ class OutTime: UIViewController {
     var searchController : UISearchController!
     @IBOutlet weak var searchView : UITableView!
     @IBOutlet fileprivate weak var maps: MKMapView!
-    //kordinate beograda
+    //kordinate beograda dao sam geografsku sirinu i duzinu da bi mapa mogla da iscrta
     let initialLocation = CLLocation(latitude: 44.816088, longitude: 20.459962)
+    //Locale je argument centralne tacke, region ce ima imati sever-jug i istog-zapad raspon od radiusa, zato radius podesavamo na 1000 metara i onda koristimo region radus na *2 jer je dobar sa unosom podataka na javnom atwork u jSon file.
     let regionRadius: CLLocationDistance = 1000
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                                                                  regionRadius * 2.0, regionRadius * 2.0)
+                                                      regionRadius * 2.0, regionRadius * 2.0)
+        //set region govori mapi da nacrta odredjenu lokaciju na osnovu kordinata koje upisujemo kao parametar func!
         maps.setRegion(coordinateRegion, animated: true)
     }
 }
@@ -39,10 +41,13 @@ extension Internal {
         
         
         
-        maps.backgroundColor = UIColor.clear
         searchView.backgroundColor = UIColor.clear
+        //poziv za search bar u navigaton item-u
         setSearch()
+        //poziv za centralnu tacku u mapi!
         centerMapOnLocation(location: initialLocation)
+        //poziv za prikazivanje odredjene tacke na osnovu kordinata i teksta unosa
+        maps.addAnnotation(artWork)
         maps.delegate = self
     }
 }
