0e7ea03bd92de0a16ece6109fa2dfa2b64769ce5
Lazar <lakiando@outlook.com>
2017-01-20 14:11:55 +0100

    new cocoa touch file subclass from collection view cell

---
+++ OutTime/ArtWorkItems.swift
@@ -0,0 +1,13 @@
+//
+//  ArtWorkItems.swift
+//  OutTime
+//
+//  Created by iosakademija on 1/20/17.
+//  Copyright © 2017 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class ArtWorkItems: UICollectionViewCell {
+    
+}
